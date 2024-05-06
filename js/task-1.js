c26b104122e2e3a6d53f5ccf55de2fdfa9ad3c82
iryna-shevchenko <iryna.shevchenko.fullstack@gmail.com>
2024-05-06 19:33:44 +0300

    Summary

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,18 @@
 'use strict';
 
-// Задача 1. Замовлення дроїдів
+// Задача 1. Пакування товарів
 
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+
+  for (let key in products) {
+    totalProducts += products[key];
+  }
+
+  return totalProducts <= containerSize;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
